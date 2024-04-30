896a72d617865c0220c70e7e5f8ffc117e8b0f0c
Drekken <drekken.pownz@protonmail.me>
2024-04-30 09:15:23 -0400

    Add DummyBot class for additional functionality in run.py

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ from sc2 import maps
 from sc2.data import AIBuild, Difficulty, Race
 from sc2.main import run_game
 from sc2.player import Bot, Computer
+from sc2.bot_ai import BotAI
 
 sys.path.append("ares-sc2/src/ares")
 sys.path.append("ares-sc2/src")
@@ -20,11 +21,25 @@ from ladder import run_ladder_game
 
 # change if non default setup / linux
 # if having issues with this, modify `map_list` below manually
-MAPS_PATH: str = "C:\\Program Files (x86)\\StarCraft II\\Maps"
+MAPS_PATH: str = "D:\\StarCraft II\\Maps"
 CONFIG_FILE: str = "config.yml"
 MAP_FILE_EXT: str = "SC2Map"
 MY_BOT_NAME: str = "MyBotName"
 MY_BOT_RACE: str = "MyBotRace"
+
+
+class DummyBot(BotAI):
+    def __init__(self):
+        super().__init__()
+
+    async def on_step(self, iteration: int):
+        target = self.game_info.map_center
+        if self.enemy_start_locations:
+            target = self.enemy_start_locations[0]
+        elif self.enemy_structures:
+            target = self.enemy_structures.first.position
+        for unit in self.units.idle:
+            unit.attack(target)
 
 
 def main():
@@ -43,6 +58,7 @@ def main():
                 race = Race[config[MY_BOT_RACE].title()]
 
     bot1 = Bot(race, MyBot(), bot_name)
+    bot2 = Bot(Race.Terran, DummyBot())
 
     if "--LadderServer" in sys.argv:
         # Ladder game started by LadderManager
@@ -52,19 +68,9 @@ def main():
     else:
         # Local game
         map_list: List[str] = [
-            p.name.replace(f".{MAP_FILE_EXT}", "")
-            for p in Path(MAPS_PATH).glob(f"*.{MAP_FILE_EXT}")
-            if p.is_file()
+             "PlateauMicro_1",
+             "BotMicroArena_6",
         ]
-        # alternative example code if finding the map path is problematic
-        # map_list: List[str] = [
-        #     "BerlingradAIE",
-        #     "InsideAndOutAIE",
-        #     "MoondanceAIE",
-        #     "StargazersAIE",
-        #     "WaterfallAIE",
-        #     "HardwireAIE",
-        # ]
 
         random_race = random.choice([Race.Zerg, Race.Terran, Race.Protoss])
         print("Starting local game...")
@@ -72,7 +78,7 @@ def main():
             maps.get(random.choice(map_list)),
             [
                 bot1,
-                Computer(random_race, Difficulty.CheatVision, ai_build=AIBuild.Macro),
+                bot2,
             ],
             realtime=False,
         )
